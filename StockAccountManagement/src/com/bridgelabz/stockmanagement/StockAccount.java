4539334fa8bfd547c42f965fcd46f6983075988a
Harsh R Jain <rjainharsh@gmail.com>
2021-09-05 16:11:00 +0530

    ADDED: buy, sell, printReport methods added to manage stock account

--- StockAccountManagement/src/com/bridgelabz/stockmanagement/StockAccount.java
+++ StockAccountManagement/src/com/bridgelabz/stockmanagement/StockAccount.java
@@ -1,46 +1,54 @@
 package com.bridgelabz.stockmanagement;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class StockAccount {
-	
+
 	private static ArrayList<Stock> stockAccount = new ArrayList<>();
-	
+	private static ArrayList<CompanyShares> transactions = new ArrayList<>();
+
 	public StockAccount() {
-		
+
 		Stock tempStock = new Stock();
-		
+
 		tempStock.setStockName("Reliance");
 		tempStock.setNumOfShares(5);
 		tempStock.setSharePrice(1200);
 		stockAccount.add(tempStock);
-		
+
 		tempStock = new Stock();
 		tempStock.setStockName("Idea");
 		tempStock.setNumOfShares(15);
 		tempStock.setSharePrice(200);
 		stockAccount.add(tempStock);
-		
+
 		tempStock = new Stock();
 		tempStock.setStockName("SBI");
 		tempStock.setNumOfShares(45);
 		tempStock.setSharePrice(100);
 		stockAccount.add(tempStock);
-		
+
 	}
-	
+
 	public void buy(int amount, String symbol) {
 		boolean flag = false;
 		for(Stock stock: stockAccount) {
 			if(stock.getStockName().equals(symbol)) {
 				stock.setNumOfShares(stock.getNumOfShares()+amount);
+				CompanyShares tempTransaction = new CompanyShares();
+				LocalDateTime dateObj = LocalDateTime.now();
+				tempTransaction.setSymbol(symbol);
+				tempTransaction.setNumOfShares(amount);
+				tempTransaction.setTransactionDateTime(dateObj);
+				transactions.add(tempTransaction);
 				flag=true;
 			}
 		}
 		if(!flag)
 			System.out.println("Stock not found");
 	}
-	
+
 	public void sell(int amount, String symbol) {
 		boolean flag = false;
 		for(Stock stock: stockAccount) {
@@ -48,14 +56,21 @@ public class StockAccount {
 				flag=true;
 				if(stock.getNumOfShares()-amount < 0) 
 					System.out.println("No enough stocks to sell");
-				else
+				else {
 					stock.setNumOfShares(stock.getNumOfShares()-amount);
+					CompanyShares tempTransaction = new CompanyShares();
+					LocalDateTime dateObj = LocalDateTime.now();
+					tempTransaction.setSymbol(symbol);
+					tempTransaction.setNumOfShares(amount*-1);
+					tempTransaction.setTransactionDateTime(dateObj);
+					transactions.add(tempTransaction);
+				}
 			}
 		}
 		if(!flag)
 			System.out.println("Stock not found");
 	}
-	
+
 	public void printReport() {
 		double valueOfStock , totalValue=0;
 		for(Stock stock : stockAccount) {
@@ -65,8 +80,15 @@ public class StockAccount {
 			System.out.println("Value of Stock is "+valueOfStock);
 		}
 		System.out.println("\nPortfolio value: "+this.valueOf());
+
+		if(transactions.size() > 0) {
+			System.out.println("\nTransactions: ");
+			for(CompanyShares transaction: transactions) {
+				System.out.println(transaction);
+			}
+		}
 	}
-	
+
 	private double calculateStockValue(Stock stock) {
 		return stock.getNumOfShares() * stock.getSharePrice();
 	}
@@ -79,5 +101,5 @@ public class StockAccount {
 		}
 		return totalValue;
 	}
-	
+
 }
--- StockAccountManagement/src/com/bridgelabz/stockmanagement/StockPortfolio.java
+++ StockAccountManagement/src/com/bridgelabz/stockmanagement/StockPortfolio.java
@@ -6,39 +6,16 @@ public class StockPortfolio {
 
 	public static void main(String[] args) {
 		
-		ArrayList<Stock> stockPortfolio = new ArrayList<>();
+		StockAccount stockAccount = new StockAccount();
+		System.out.println("Initial portfolio: ");
+		stockAccount.printReport();
+		System.out.println();
 		
-		Stock tempStock = new Stock();
+		stockAccount.buy(10, "Reliance");
+		stockAccount.sell(3, "Idea");
 		
-		tempStock.setStockName("Reliance");
-		tempStock.setNumOfShares(5);
-		tempStock.setSharePrice(1200);
-		stockPortfolio.add(tempStock);
+		stockAccount.printReport();
 		
-		tempStock = new Stock();
-		tempStock.setStockName("Idea");
-		tempStock.setNumOfShares(15);
-		tempStock.setSharePrice(200);
-		stockPortfolio.add(tempStock);
-		
-		tempStock = new Stock();
-		tempStock.setStockName("SBI");
-		tempStock.setNumOfShares(45);
-		tempStock.setSharePrice(100);
-		stockPortfolio.add(tempStock);
-		
-		double valueOfStock , totalValue=0;
-		for(Stock stock : stockPortfolio) {
-			System.out.println(stock);
-			valueOfStock=calculateStockValue(stock);
-			totalValue+=valueOfStock;
-			System.out.println("Value of Stock is "+valueOfStock);
-		}
-		System.out.println("\nPortfolio value: "+totalValue);
-	}
-
-	private static double calculateStockValue(Stock stock) {
-		return stock.getNumOfShares() * stock.getSharePrice();
 	}
 
 }
